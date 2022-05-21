bbddb310fdd46aacc90d15bcfd6d8f926b100bd5
girin-sj <radianttwilight24@gmail.com>
2022-05-21 13:47:35 +0900

    UWang

---
+++ Assets/Scripts/StageMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageMenu : MonoBehaviour
+{
+    // 단군신화 버튼
+    public void ClickDangun()
+    {
+        print("단군신화 버튼 누름");
+    }
+
+    // 고조선 버튼
+    public void ClickGojoseon()
+    {
+        print("고조선 버튼 누름");
+    }
+
+    // 삼국시대 버튼
+    public void ClickThreeState()
+    {
+        print("삼국시대 버튼 누름");
+    }
+
+    // 통일신라 버튼
+    public void ClickUnifiedSilla()
+    {
+        print("통일신라 버튼 누름");
+    }
+
+    // 고려 버튼
+    public void ClickKoryo()
+    {
+        print("고려 버튼 누름");
+    }
+
+    // 조선 버튼
+    public void ClickJoseon()
+    {
+        print("조선 버튼 누름");
+    }
+
+    // 일제강점기 버튼
+    public void ClickJapanOccupation()
+    {
+        print("일제강점기 버튼 누름");
+    }
+
+    // 현대사 버튼
+    public void ClickMordernHistory()
+    {
+        print("고조선 버튼 누름");
+    }
+}
