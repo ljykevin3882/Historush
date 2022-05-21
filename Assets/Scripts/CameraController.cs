8ce06081b0d89ec3d80bc5ccdd3092f39edc2fc3
JY <ljykevin3882@gmail.com>
2022-05-21 18:02:02 +0900

    LSJ+LJY

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Define.CameraMode _mode = Define.CameraMode.QuaterView;
+
+    [SerializeField]
+    Vector3 _delta;
+
+    [SerializeField]
+    Player_Move _player;
+    void LateUpdate()
+    {
+        if (_mode == Define.CameraMode.QuaterView)
+        {
+            transform.position = _player.transform.position + _delta;
+            transform.LookAt(_player.transform);
+        }
+    }
+}
