0d21530ea61340896ff327beb040c73654aaea99
girin-sj <radianttwilight24@gmail.com>
2022-05-21 16:14:20 +0900

    0521

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -12,6 +12,7 @@ public class Player_Move : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Animator anim;
     CapsuleCollider2D capsuleCollider;
+    public float speed_Character = 5f;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -23,50 +24,68 @@ public class Player_Move : MonoBehaviour
     private void Update()
     {
         //Move by control
-        float h = 20.0f;
+        float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        anim.SetBool("IsWalking", true);
-        if (rigid.velocity.x > maxSpeed) //오른쪽
+
+
+        if (rigid.velocity.x > maxSpeed) //오른쪽 
         {
+            anim.SetBool("IsWalking", true);
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+
         }
-        /*
+
         else if (rigid.velocity.x < maxSpeed * (-1)) //왼쪽
         {
+            anim.SetBool("IsWalking", true);
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-        }*/
+
+        }
+
+        //슬라이드
+        if (Input.GetButtonDown("Slide"))
+        {
+            transform.Rotate(0, 0, 90);
+
+        }
+        if (Input.GetButtonUp("Slide"))
+        {
+            transform.Rotate(0, 0, -90);
+
+        }
         //점프할때
-        if (Input.GetButtonDown("Jump")&&jumpCount>0)//&&!anim.GetBool("isJumping")
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)//&&!anim.GetBool("isJumping")
         {
             jumpCount--;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping", true);
         }
         //방향키 떼서 멈출때 
-        /*
+
         if (Input.GetButtonUp("Horizontal"))
         {
 
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-        }*/
+        }
         //방향전환
-        /*
+
         if (Input.GetButton("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
 
-        if (rigid.velocity.x <0.3&& rigid.velocity.x > -0.3) //Math.abs(rigid.velocity.x)<0.3 과 같은 코드
+        if (rigid.velocity.x < 0.3 && rigid.velocity.x > -0.3) //Math.abs(rigid.velocity.x)<0.3 과 같은 코드
         {
             anim.SetBool("IsWalking", false);
         }
         else
         {
             anim.SetBool("IsWalking", true);
-        }*/
+        }
     }
 
     private void FixedUpdate()
     {
-      
+
         //LandingPlatform
         if (rigid.velocity.y < 0)
         {
@@ -88,17 +107,24 @@ public class Player_Move : MonoBehaviour
 
         if (collision.gameObject.tag == "Enemy")
         {
-            if(rigid.velocity.y < 0&&transform.position.y>collision.transform.position.y)
+            if (collision.gameObject.name == "Enemy")
             {
-                OnAttack(collision.transform);
+                if (rigid.velocity.y <= 0 && transform.position.y > collision.transform.position.y)
+                {
+                    OnAttack(collision.transform);
+                }
+                else
+                {
+                    OnDamaged(collision.transform.position);
+                }
             }
             else
             {
                 OnDamaged(collision.transform.position);
+
             }
-            
         }
-        
+
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -111,16 +137,16 @@ public class Player_Move : MonoBehaviour
 
             if (isBronze)
                 gameManager.stagePoint += 50;
-            else if(isSilver)
+            else if (isSilver)
                 gameManager.stagePoint += 100;
-            else if(isGold)
+            else if (isGold)
                 gameManager.stagePoint += 300;
 
-           
+
             //아이템 사라지기
             collision.gameObject.SetActive(false);
         }
-        else if(collision.gameObject.tag == "Finish")
+        else if (collision.gameObject.tag == "Finish")
         {
             //다음 스테이지
             gameManager.NextStage();
@@ -138,17 +164,22 @@ public class Player_Move : MonoBehaviour
     void OnDamaged(Vector2 targetPos)
     {
         //체력감소
-        gameManager.HealthDown();
-        //래이어 변경
-        gameObject.layer = 11;
-        //색변경
-        spriteRenderer.color = new Color(1, 1, 1,0.4f);
-        //넉백
-        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
-        rigid.AddForce(new Vector2(dirc, 1)*7, ForceMode2D.Impulse);
-        //애니메이션
-        anim.SetTrigger("doDamaged");
-        Invoke("OffDamaged", 2);
+        if (gameObject.layer == 10)
+        {
+            gameManager.HealthDown();
+
+
+            //래이어 변경
+            gameObject.layer = 11;
+            //색변경
+            spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+            //넉백
+            int dirc = transform.position.x - targetPos.x >= 0 ? 1 : -1;
+            rigid.AddForce(new Vector2(dirc, 1) * 7, ForceMode2D.Impulse);
+            //애니메이션
+            anim.SetTrigger("doDamaged");
+            Invoke("OffDamaged", 2);
+        }
     }
     void OffDamaged()
     {
--- Assets/Scripts/StageMenu.cs
+++ Assets/Scripts/StageMenu.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageMenu : MonoBehaviour
 {
+    public Color black;
+    public Color white;
+    public Image[] ButtonImages;
+    public GameObject[] LockImages;
+    public int stage_phase = -1;
+
+    public void Start()
+    {
+        //처음 단군신화 빼고 다 잠금
+        if (stage_phase == -1)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                ButtonImages[i].color = black;
+                LockImages[i].gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void Update()
+    {
+        //스테이지 클리어후, 버튼 색 변경 및 자물쇠 해금
+        for (int i = 0; i < 8; i++)
+        {
+            if (stage_phase == i)
+            {
+                ButtonImages[i].color = new Color (1, 1, 1, 1);
+                LockImages[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+
     // 단군신화 버튼
     public void ClickDangun()
     {
